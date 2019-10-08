aed4436b35b57fa85f78f0431542f223232cfb3f
VNHZero <thach.hodang951@gmail.com>
2019-10-08 15:53:15 +0700

    upload bai tap

---
+++ ss1 lap trinh can ban/Sử dụng toán tử/script.js
@@ -0,0 +1,8 @@
+let CR;
+let RD;
+CR = prompt("Nhập Chiều Rộng")
+CD = prompt("Nhập Chiều Dài")
+let R = parseInt(CR);
+let D = parseInt(CD);
+let S = D * R;
+document.write("Diện Tích Hình Chữ Nhật Là : " + S);
